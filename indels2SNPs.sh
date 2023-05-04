2b107428aea6d909a87280113be51f7a7ea1329d
Jacob Marsh <51686617+jacobimarsh@users.noreply.github.com>
2023-05-04 15:03:59 +0800

    Update indels2SNPs.sh

--- indels2SNPs.sh
+++ indels2SNPs.sh
@@ -1,1 +1,7 @@
 
+#extract indels only from VCF
+vcftools --vcf in.vcf --keep-only-indels --recode
+
+#clean FORMAT to only keep genotype information for each allele
+bcftools annotate -x 'fmt' out.recode.vcf > clean_indels.vcf
+
